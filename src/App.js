58ec8d9fc8d5926db4e2573460e90c67fa4cdeaf
bhavana1308 <bonamukkala.bhavana@gmail.com>
2024-03-11 13:31:47 -0500

    app.js file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   return (
     <Routes>
        <Route path='/' element={<LandingPage />} />
-      
       <Route path='/api/register' element={<Register />} />
       <Route path='/api/Login' element={<Login />} />
      </Routes>
