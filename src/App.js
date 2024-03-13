af224ee76c859f796bbdf1fec44e6a64e1c19ef8
bhavana1308 <bonamukkala.bhavana@gmail.com>
2024-03-13 08:04:07 -0500

    added logout in app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
 import LandingPage from './components/LandingPage';
+import Logout from './components/Logout';
+import LogoutSuccess from './components/LogoutSuccess';
 
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
        <Route path='/' element={<LandingPage />} />
       <Route path='/api/register' element={<Register />} />
       <Route path='/api/Login' element={<Login />} />
+      <Route path='/api/logout' element={<Logout />} />
+      <Route path='/api/logout-success' element={<LogoutSuccess />} />
      </Routes>
   );
 }
