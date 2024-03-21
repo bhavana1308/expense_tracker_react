3b53942e3f91d616fc79d75ac472bb7ac0f9da4a
bhavana1308 <bonamukkala.bhavana@gmail.com>
2024-03-21 11:40:19 -0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import AddExpense from './components/AddExpense';
 import EditExpense from './components/EditExpense';
 import Logout from './components/Logout';
 import LogoutSuccess from './components/LogoutSuccess';
-
+import ExpenseList from './components/ExpenseList';
 
 
 function App() {
@@ -16,9 +16,11 @@ function App() {
       <Route path='/api/register' element={<Register />} />
       <Route path='/api/Login' element={<Login />} />
       <Route path="/api/expense/add" element={<AddExpense />} />
+      <Route path="/api/expense/list" element={<ExpenseList/>}/>
       <Route path="/api/expense/edit/:expenseId" element={<EditExpense />} />
       <Route path='/api/logout' element={<Logout />} />
       <Route path='/api/logout-success' element={<LogoutSuccess />} />
+     
      </Routes>
   );
 }
